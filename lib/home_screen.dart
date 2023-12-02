56624992cb799c936959ec76ee391709eafb2177
Asim Majeed <104228302+a4-asim@users.noreply.github.com>
2023-12-03 01:22:54 +0500

    Snack Bar Check

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-
-
-    return  Scaffold(
-      appBar: AppBar(title:Text('Getx Learning'),),
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Getx Learning'),
+      ),
+      body: Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Get.snackbar(
+          "Asim Majeed",
+          "Welcome to Getx learning app",
+          backgroundColor: Colors.blueAccent,
+        );
+      }),
     );
   }
 }
