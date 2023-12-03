b16427abdc49e50cffd2d8646292c5449b556114
Asim Majeed <104228302+a4-asim@users.noreply.github.com>
2023-12-03 14:24:02 +0500

    Dialogue box and Bottomsheet with theme changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,16 +13,71 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Getx Learning'),
+        title: const Text('Getx Learning'),
       ),
       body: Column(
-        children: [],
+        children: [
+          Card(
+              child: ListTile(
+            title: const Text("Getx Dialogue Box"),
+            onTap: () {
+              Get.defaultDialog(
+                contentPadding: const EdgeInsets.all(20),
+                titlePadding: const EdgeInsets.all(10),
+                title: 'Delete Chat',
+                middleText: 'Are your sure?',
+                confirm: TextButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text('Ok')),
+                cancel: TextButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text('Cancel')),
+              );
+            },
+          )),
+          Card(
+            child: ListTile(
+              title: const Text("Getx Bottom Sheet"),
+              onTap: () {
+                Get.bottomSheet(Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 85, 156, 238),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.light_mode),
+                        title: const Text('Light Mode'),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.dark_mode),
+                        title: const Text('Dark Mode'),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                      ),
+                    ],
+                  ),
+                ));
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         Get.snackbar(
           "Asim Majeed",
           "Welcome to Getx learning app",
           backgroundColor: Colors.blueAccent,
+          icon: const Icon(Icons.show_chart),
         );
       }),
     );
